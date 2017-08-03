dc4c72a3214ed06ca662b96587039910429ac5ff
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-03 04:54:21 +0200

    add comments to renderer code

--- case-study/caseStudy/src/casestudy/rendering/Client.java
+++ case-study/caseStudy/src/casestudy/rendering/Client.java
@@ -7,20 +7,22 @@ public class Client {
 
 	public static void main(String[] args){
 	
-		ComposedWorldObject root = new ComposedWorldObject("root");
+		ComposedWorldObject root = new ComposedWorldObject("root");// define a root world object
 
 		
 		
-		RenderingSystem renderingSystem = new RenderingSystem();
+		RenderingSystem renderingSystem = new RenderingSystem();//defines a rendering object
 		
-		WorldObjectRenderer renderer = new QuickRenderer();
-		WorldObjectRenderer finalImageRenderer = new DetailedRenderer();
+		WorldObjectRenderer renderer = new QuickRenderer();// defines a world object renderer
+		WorldObjectRenderer finalImageRenderer = new DetailedRenderer();//defines a detaild renderer
 
-		renderingSystem.setWorldObjectRenderer(renderer);
-		renderingSystem.render(root);
+		renderingSystem.setWorldObjectRenderer(renderer);//add quik renderer to 
+                                                                //the renering system object
+		renderingSystem.render(root);// render the world object using quik renderer
 		
-		renderingSystem.setWorldObjectRenderer(finalImageRenderer);
-		renderingSystem.render(root);
+		renderingSystem.setWorldObjectRenderer(finalImageRenderer);// add detaild renderer object
+                                                                           //to the rendering system object
+		renderingSystem.render(root);//render the world object using the detailed renderer
 		
 	}
 	
