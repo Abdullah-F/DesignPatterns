618f66395bb8fb5952c0d077692b8740b82d5164
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-08 03:18:50 +0200

    comment multiplatform pattern

--- case-study/caseStudy/src/casestudy/multiplatform/improved/IconWindow.java
+++ case-study/caseStudy/src/casestudy/multiplatform/improved/IconWindow.java
@@ -7,8 +7,8 @@ public class IconWindow extends Window{
 	}
 
 	public void drawIconWindow(){
-		drawWindow();
-		System.out.println("Draw Icon");
+		drawWindow();//calls the parent method which delegates the call to the bridge object
+		System.out.println("Draw Icon");//draws an icon window
 	}
 
 }
--- case-study/caseStudy/src/casestudy/multiplatform/improved/Window.java
+++ case-study/caseStudy/src/casestudy/multiplatform/improved/Window.java
@@ -3,14 +3,20 @@ package casestudy.multiplatform.improved;
 public abstract class Window {
 
 	// this is the bridge pattern, and the iwindowimpl contains the drawwWindow fuctionality
-	private WindowImpl impl;
+	private WindowImpl impl;//a bridge between Windos types and their 
+                               ////implementaion in different platforms
 	
-	public Window(WindowImpl impl){
+	public Window(WindowImpl impl){// Window constructor
 		this.impl = impl;
 	}
 	
-	public void drawWindow(){
-		impl.drawWindow();
+	public void drawWindow(){// now this method is shared between all children
+                                 // and no method reption
+                                 // so this method delegates the responsiblity to the specialized 
+                                 // platform acting as a bridge 
+		impl.drawWindow();//implements the window drawing based on the bridge object above
+                                  // if impl is for mac then it draws the window for mac platform
+                                  // else if impl is a windows object it draws the window for windows
 	}
 
 	public void setImpl(WindowImpl impl) {
