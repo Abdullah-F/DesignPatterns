f1d419e9968fe350c37fbac8620a6997aaf391f2
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-13 07:19:59 +0200

    add comments to problematic code

--- singiltonPattern/src/problem/Client.java
+++ singiltonPattern/src/problem/Client.java
@@ -1,14 +1,14 @@
-package creational.singleton;
+package problem;
 
 
 public class Client {
 
     public static void main(String[] args) {
 
-        System.out.println(Preferences.globalPreferences.getPreferencesDetail());
+        System.out.println(Preferences.globalPreferences.globalPreferences.getPreferencesDetail());
 
-        Preferences secondPreference = new Preferences("Second preference");
-
+        Preferences secondPreference = new Preferences("Second preference");// can be defined again and agin !
+        
         Preferences.globalPreferences = secondPreference;
 
         System.out.println(Preferences.globalPreferences.getPreferencesDetail());
--- singiltonPattern/src/problem/Preferences.java
+++ singiltonPattern/src/problem/Preferences.java
@@ -1,9 +1,10 @@
-package creational.singleton;
+package problem;
 
 
 public class Preferences {
 
-    public static Preferences globalPreferences = new Preferences("First one created");
+    public static Preferences globalPreferences = new Preferences("First one created");//this is the problem
+    //we need to create a global varaible but the problem that it can be defined again and again
 
     private String preference;
 
