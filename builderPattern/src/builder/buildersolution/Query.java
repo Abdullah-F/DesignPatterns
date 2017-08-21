bbf8ee42214639b52b5ec471e522f5c662860bd3
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-21 23:07:38 +0200

    add more comments

--- builderPattern/src/builder/buildersolution/Query.java
+++ builderPattern/src/builder/buildersolution/Query.java
@@ -1,5 +1,7 @@
 package builder.buildersolution;
-
+/*** 
+   a query interface used for run time switching between different query types (sql, mongo, ..etc)  
+ ***/
 public interface Query {
 
 	void execute();
--- builderPattern/src/builder/buildersolution/QueryBuilder.java
+++ builderPattern/src/builder/buildersolution/QueryBuilder.java
@@ -1,5 +1,9 @@
 package builder.buildersolution;
 
+/*** 
+          query builder interface that provides run time switching between different query builders
+          like (mongo builder, sql builder, ..etc) 
+  ***/
 public interface QueryBuilder {
 
 	void from(String from);
