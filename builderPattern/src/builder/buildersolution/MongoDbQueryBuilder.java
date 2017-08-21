364c2c27c0bf6ddcf7225dc8db0d668ab587a77f
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-21 23:04:47 +0200

    add comments

--- builderPattern/src/builder/buildersolution/MongoDbQueryBuilder.java
+++ builderPattern/src/builder/buildersolution/MongoDbQueryBuilder.java
@@ -1,8 +1,10 @@
 package builder.buildersolution;
-
+/**
+this class is responsible only for building a mongo query object and it returns a mongo query object
+**/
 public class MongoDbQueryBuilder implements QueryBuilder{
 
-	private MongoDbQuery query = new MongoDbQuery();
+	private MongoDbQuery query = new MongoDbQuery();// defining a mongo query object
 	
 	@Override
 	public void from(String from) {
@@ -16,7 +18,7 @@ public class MongoDbQueryBuilder implements QueryBuilder{
 
 	@Override
 	public Query getQuery() {
-		return query;
+		return query;// returns the built mongo query object which will be used to execute the query
 	}
 
 }
