994d8b9b48501a4e127fb37b204699cca6dd6fd3
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-21 23:02:11 +0200

    adding builder solution

--- builderPattern/src/builder/buildersolution/Client.java
+++ builderPattern/src/builder/buildersolution/Client.java
@@ -5,9 +5,18 @@ public class Client {
 	public static void main(String[] args){
 		
 		// setup the director
-		QueryBuildDirector director = new QueryBuildDirector();
-		String from="client table";
-		String where="client name = ...";
+		QueryBuildDirector director = new QueryBuildDirector(); // setting up a query builder director
+		String from="client table"; // setting query from clause
+		String where="client name = ..."; // setting query where clause
+                
+                QueryBuilder mongoQueryBuilder = new MongoDbQueryBuilder();// setting a mongo query builder
+                QueryBuilder sqlQueryBuilder = new SqlQueryBuilder();// setting sql query builder
+                
+                Query mongoquery = director.buildQuery(from, where, mongoQueryBuilder);// building mongo query
+                mongoquery.execute();// executing mongo query !
+                
+                Query sqlquery = director.buildQuery(from, where, sqlQueryBuilder);// building sql query
+                sqlquery.execute();// executing sql query
 		
 
 
