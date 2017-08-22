057e2399f1b900d515d2b22f2c7ad316f5dcf5f1
Abdullah-F <abdullahfadhel@outlook.com>
2017-08-23 00:42:52 +0200

    completing and commenting the problem code

--- factoryMethod/src/factorymethod/problem/Car.java
+++ factoryMethod/src/factorymethod/problem/Car.java
@@ -2,4 +2,14 @@ package factorymethod.problem;
 
 
 public class Car extends Transport {
+/*
+    this class extends the transport interface for transport 
+    we can add it its implementation as we need
+    **/
+    
+    @Override
+    public String drive()
+    {
+        return "driving a car";
+    }
 }
