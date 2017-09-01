e8e23a9129c71971b61d797932cc7f24bcf42b29
Abdullah-F <abdullahfadhel@outlook.com>
2017-09-01 04:45:10 +0200

    add comments to problem code

--- facadePattern/src/facadepattern/problem/Client.java
+++ facadePattern/src/facadepattern/problem/Client.java
@@ -6,12 +6,16 @@ public class Client {
 
 	public static void main(String [] args){
 		
-         BillingSystem billingSystem = new BillingSystem();
-        InvoiceCustomerSystem invoiceCustomerSystem = new InvoiceCustomerSystem();
+        BillingSystem billingSystem = new BillingSystem();// creating a billing system
+        InvoiceCustomerSystem invoiceCustomerSystem = new InvoiceCustomerSystem();//client manages here
+                                                                                  //to create the invoice
+                                                                                  //system also !
 
 
-        Bill bill = billingSystem.createBill(1000);
-        invoiceCustomerSystem.createInvoiceForBill(bill);
+        Bill bill = billingSystem.createBill(1000);//the client code here also creates the bill !
+        invoiceCustomerSystem.createInvoiceForBill(bill);// finally an invoice is created for the bill
+                                                        //and the client code is title coupled
+                                                        // to all sort of details !
 		
 		
 	}
