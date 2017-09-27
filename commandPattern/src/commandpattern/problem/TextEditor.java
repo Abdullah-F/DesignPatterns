51586e727e3ba41f472838ff7debdb7ae22ddf67
Abdullah-F <abdullahfadhel@outlook.com>
2017-09-28 00:56:25 +0200

    add problem code

--- commandPattern/src/commandpattern/problem/TextEditor.java
+++ commandPattern/src/commandpattern/problem/TextEditor.java
@@ -10,5 +10,36 @@ package commandpattern.problem;
  * @author abdullah-fadhel
  */
 public class TextEditor {
+    private String text;
+    private String undoText;
     
+    public TextEditor(String text){
+        this.text = text;
+    }
+    public void addText(String text){
+        this.undoText = this.text;
+        this.text = text;
+    }
+    
+    public void clearText()
+    {
+        this.undoText = text;
+        this.text = "";
+    }
+    
+    public String getText(){
+        return this.text;
+    }
+    
+    public void undo(){
+        this.text = undoText;
+    }
+    
+    public static void main(String[] args){
+        TextEditor textEditor = new TextEditor("");
+        System.out.println(textEditor.getText());
+        textEditor.addText("some Text");
+        textEditor.undo();
+        System.out.println(textEditor.getText());
+    }
 }
