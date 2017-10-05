f13159dbabc53e81c2490f337335686bf24eeda7
Abdullah-F <abdullahfadhel@outlook.com>
2017-10-05 08:15:58 +0200

    commen client code

--- observerPattern/src/observerpattern/solution/Client.java
+++ observerPattern/src/observerpattern/solution/Client.java
@@ -4,16 +4,16 @@ package observerpattern.solution;
 public class Client {
 
     public static void main(String[] args) {
-        Button button = new Button();
-        InputText text = new InputText();
-        List list = new List();
+        Button button = new Button();// creates a button
+        InputText text = new InputText();//creates an input text
+        List list = new List();//creates a list
 
 
 
-        button.attachObserver(text);
-        button.attachObserver(list);
+        button.attachObserver(text);//adds text as an observer
+        button.attachObserver(list);//assd list as an observer
 
-        button.clicked();
+        button.clicked();//the click event triggers the observers !
 
     }
 
